17a13c49311d34b1727b12f650b0701272c30493
ghigbie <gehigbie@gmail.com>
2021-12-18 19:07:45 -0500

    Add new route

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ def root():
 @app.get('/blog/{id}')
 def get_blog(id: int):
   return {"message": f"blog with id: {id}"}
+
+@app.get('/blog/all/')
+def get_all_blogs():
+  return {"message": "These are all blogs"}
